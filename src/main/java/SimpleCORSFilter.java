c00c491dba0e247b882cf7fd0d94ddfdd2bb0e52
DeMiTeLi <dmitrii1790@gmail.com>
2015-11-01 13:42:49 +0300

    init commit

---
+++ src/main/java/SimpleCORSFilter.java
@@ -0,0 +1,30 @@
+package main.java;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+/**
+ * Created by ramansilin on 10/30/15.
+ */
+public class SimpleCORSFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        if ("OPTIONS".equals(request.getMethod())) {
+            // CORS "pre-flight" request
+            response.addHeader("Access-Control-Allow-Origin", "*");
+            response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+            response.addHeader("Access-Control-Allow-Headers", "Content-Type");
+            response.addHeader("Access-Control-Max-Age", "1800");//30 min
+            System.out.println(":::: OPRIONS()  :::" );
+
+        }
+        filterChain.doFilter(request, response);
+        System.out.println(":::: OPRIONS()  :::" );
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/jsp/hello.jsp
@@ -0,0 +1,10 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ramansilin
+  Date: 10/29/15
+  Time: 5:56 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+ Hello
